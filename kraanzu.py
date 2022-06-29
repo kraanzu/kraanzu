d7ffee4e29fd7a383578501e4f527301a7542d99
kraanzu <kraanzu@gmail.com>
2022-06-29 08:33:58 +0530

    Update Stuff

--- kraanzu.py
+++ kraanzu.py
@@ -8,21 +8,28 @@ console = Console(record=True, width=100)
 
 tree = Tree("🤓 Murli Tawari", guide_style="bold cyan")
 python_tree = tree.add("🐍 Python dev", guide_style="green")
+python_tree.add("🗒️ [link=https://github.com/kraanzu/dooit]Dooit")
 python_tree.add("💬 [link=https://github.com/kraanzu/gupshup]Gupshup")
 python_tree.add("💻 [link=https://github.com/kraanzu/termtyper]Termtyper")
 
-competitive_tree = tree.add("🔧 Competitive Programmer")
-competitive_tree.add("⭐ [link=https://codeforces.com/profile/easedeath]Codeforces")
-competitive_tree.add("⭐ [link=https://leetcode.com/easedeath/]Leetcode")
+competitive_tree = tree.add("🔧 Sport Programmer")
+competitive_tree.add("⭐ [link=https://codeforces.com/profile/kraanzu]Codeforces")
+competitive_tree.add("⭐ [link=https://leetcode.com/kraanzu/]Leetcode")
 
 about = """\
-I'm a sophomore pursuring BTECH-CSE. I love trying out new technologies.
-I am currently developing different applications with [link=https://www.python.org/]Python[/] and I am learning [link=https://www.rust-lang.org/]Rust[/] and [link=https://www.typescriptlang.org/]Typescript[/].
-Oh and, I use Arch btw
+I'm a sophomore pursuring bachelors in Computer Science.
+I love sport programming and tinkering with different techs.
+
+I'm currently making TUI applications with [link=https://www.python.org/]Python[/] and will expand this to other languages as well
+Oh and, I use Arch btw ;) \
 """
 
 panel = Panel.fit(
-    about, box=box.DOUBLE, border_style="blue", title="[b]Hi there", width=60
+    about,
+    box=box.DOUBLE,
+    border_style="green",
+    title="[b]Hi there fellas!",
+    width=60,
 )
 
 console.print(Columns([panel, tree]))
