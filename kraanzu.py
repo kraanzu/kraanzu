910b2be3064afcabfbdb3d08a8e65ea898d2af90
kraanzu <kraanzu@gmail.com>
2022-07-02 19:37:58 +0530

    better padding

--- kraanzu.py
+++ kraanzu.py
@@ -19,7 +19,6 @@ competitive_tree.add("⭐ [link=https://leetcode.com/kraanzu/]Leetcode")
 about = """\
 I'm a sophomore pursuing bachelors in Computer Science.
 I love sport programming and tinkering with different technologies.
-
 I'm currently making TUI applications with [link=https://www.python.org/]Python[/] and will expand this to other languages as well
 Oh and, I use Arch btw ;) \
 """
